c26d6bf77637b8e62157260145c3d30ce2575af8
Marcelo Amador <marcelojaf@gmail.com>
2017-09-15 17:21:47 -0400

    Module 3

---
+++ WebApplication2/WebApplication2/Controllers/TicketsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class TicketsController : Controller
+    {
+        [HttpGet("/tix")]
+        public IActionResult Index()
+        {
+            //GO TO THE DATABASE
+            //GET SOME MODELS (STUFF)
+            var s = new Seat()
+            {
+                Location = "Orchestra",
+                Price = 300.00
+            };
+
+
+            return View(s);
+        }
+
+        public string Index2()
+        {
+            return "Hello from Tickets!";
+        }
+    }
+}

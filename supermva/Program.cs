477b4952f096be9e307d935a558f974ef1dfa9e0
Marcelo Amador <marcelojaf@gmail.com>
2017-09-14 23:32:10 -0400

    First video

---
+++ supermva/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using movingtotheweb;
+
+namespace supermva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			var host = new WebHostBuilder()
+				.UseKestrel()
+				.UseStartup<Startup>()
+				.Build();
+			
+			host.Run();
+        }
+    }
+}

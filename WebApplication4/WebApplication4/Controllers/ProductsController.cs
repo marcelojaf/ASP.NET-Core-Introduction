833d97fd9021d6ef3f95279bfea12984591c3269
Marcelo Amador <marcelojaf@gmail.com>
2017-09-18 22:20:55 -0400

    Module 5

---
+++ WebApplication4/WebApplication4/Controllers/ProductsController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Products")]
+    public class ProductsController : Controller
+    {
+        private static List<Product> _products = new List<Product>(new[]
+            {
+                new Product() { ID = 1, Name = "Green Peppers"},
+                new Product() { ID = 2, Name = "Soft Taco"},
+                new Product() { ID = 3, Name = "Chipotle Sauce"}
+            });
+
+        [HttpGet]
+        public List<Product> Get()
+        {
+            return _products; //pretend to go to the database
+        }
+
+        [HttpGet("{id}")] //capture route parameter
+        public IActionResult Get(int id)
+        {
+            var product = _products.SingleOrDefault(p => p.ID == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(product);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            _products.Add(product);
+            return CreatedAtAction(nameof(Get),
+                new { id = product.ID }, product);
+        }
+    }
+}
